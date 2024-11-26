6a9e5952ebd63ccaad345cbc37072e552154681b
smith4545454545454556 <smith.maharjan1233@gmail.com>
2024-11-26 17:11:17 +0545

    message

---
+++ vercel_backend/app.js
@@ -0,0 +1,23 @@
+import express, { json } from "express"
+import cors from "cors"
+import { router } from "./src/route/dataRoutes.js";
+import dotenv from "dotenv"
+const app = express()
+const corsOptions = {
+    origin: process.env.FRONTEND_ORIGIN, // Allow your frontend
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],  // Allowed HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,              // Allow sending cookies or Authorization header
+    // Allowed headers
+    optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions))
+app.options("*", cors(corsOptions));
+
+
+app.use(express.json())
+app.get("/", (req, res) => {
+    res.send("hello")
+})
+app.use(router)
+export default app
